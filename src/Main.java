156a7e273a735956d01265d90097d87fe9dd9c69
daniilakulov <sladkoeshka33@yandex.ru>
2022-08-29 10:19:17 +0700

    Снова переделал

--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,28 @@ public class Main {
 
     }
 
-    private static boolean leapYear(int year) {
+    private static void leapYear(int year) {
         if ((year % 400 == 0) || ((year % 100 != 0 & (year % 4 == 0)))){
             System.out.println(year + " - високосный год");
         }else{
             System.out.println(year + " - не високосный год");
         }
-        return true;
     }
 
     public static void task1() {
-        int year = 2024;
-        leapYear(year);
+        leapYear(2024);
 
 
     }
 
-    public static void deviceClient(int OS, int deviceYear) {
+    public static void deviceClient(int os, int deviceYear) {
         int currentYear = 2015;
 
         String versionMessage = deviceYear < currentYear ? "облегченную" : "";
-        String osMessage = OS == 0 ? "IOS" : "Android";
+        String osMessage = os == 0 ? "IOS" : "Android";
 
         System.out.printf("Установите %s версию приложения для %s по ссылке", versionMessage, osMessage);
-        System.out.println(" ");
+        System.out.println();
     }
 
 
@@ -41,21 +39,22 @@ public class Main {
         deviceClient(0, 2016);
     }
 
-    public static int diliveryDistance(int deliveryDays) {
-        int deliverydistance = 95;
-        if (deliverydistance > 20) {
+    public static int diliveryDistance(int distance) {
+        int deliveryDays = 1;
+        int firstDistance = 20;
+        int nextDistance = 60;
+        if (distance > firstDistance) {
             deliveryDays++;
         }
-        if (deliverydistance > 60) {
+        if (distance > nextDistance) {
             deliveryDays++;
         }
         System.out.println("Потребуется дней " + deliveryDays);
-        return deliverydistance;
+        return distance;
     }
 
     public static void task3() {
-        int deliveryDays = 1;
-        diliveryDistance(deliveryDays);
+        diliveryDistance(95);
 
     }
 }
